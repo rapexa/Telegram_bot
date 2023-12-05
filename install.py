6fe5ec08e8ed210679ef13ae5ce0a26c84e04512
rapexa <rapexamrs@gmail.com>
2023-12-05 06:48:34 +0330

    make some fixes for servers

--- install.py
+++ install.py
@@ -1,10 +1,10 @@
 import os
 
-os.system('pip install --upgrade pip')
-os.system('pip install urllib3==1.25.11')
-os.system('pip install python-telegram-bot==13.15')
-os.system('pip install telethon')
-os.system('pip install jdatetime')
-os.system('pip install pymysql')
-os.system('pip install requests')
-os.system('pip install psutil')
+os.system('pip3 install --upgrade pip')
+os.system('pip3 install urllib3==1.25.11')
+os.system('pip3 install python-telegram-bot==13.15')
+os.system('pip3 install telethon')
+os.system('pip3 install jdatetime')
+os.system('pip3 install pymysql')
+os.system('pip3 install requests')
+os.system('pip3 install psutil')
